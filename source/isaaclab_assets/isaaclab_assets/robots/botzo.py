2fa74f6d9df169bf06d3d40bebe54215c247c3fd
GRINGOLOCO7 <gorlando.ieu2022@student.ie.edu>
2025-10-06 22:08:46 +0200

    set up Botzo RL

--- source/isaaclab_assets/isaaclab_assets/robots/botzo.py
+++ source/isaaclab_assets/isaaclab_assets/robots/botzo.py
@@ -1,59 +1,60 @@
 import isaaclab.sim as sim_utils
-from isaaclab.actuators import ImplicitActuatorCfg
+from isaaclab.actuators import ActuatorNetMLPCfg, DCMotorCfg, ImplicitActuatorCfg
 from isaaclab.assets.articulation import ArticulationCfg
+import os
+from math import pi
 
-stiffness = 2000.0     # N⋅m/rad - Very stiff for standing
-damping = 200.0        # N⋅m⋅s/rad - High damping for stability
-effort_limit_sim = 4.0 # N⋅m - Allow higher than rated for simulation
-velocity_limit_sim = 10.0 # rad/s - Slightly higher for responsiveness
-
-stiffness *= 40
-damping *= 15
-effort_limit_sim *= 25
-velocity_limit_sim *= 10
+BOTZO_ACTUATOR_CFG = ImplicitActuatorCfg(
+    joint_names_expr=[
+        ".*HAA",  # hip abduction/adduction
+        ".*HFE",  # hip flexion/extension
+        ".*KFE",  # knee flexion/extension
+    ],
+    effort_limit=80.0,      # ← allows enough torque to hold weight (try 400–800)
+    velocity_limit=7.0,     # ← safe high limit
+    stiffness=40.0,        # ← stronger position control (acts like motor Kp)
+    damping=5.0,            # ← reasonable damping (acts like Kd)
+)
 
 BOTZO_CONFIG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
-        usd_path="C:\\Users\\grego\\Desktop\\GRINGO\\botzo\\botzo\\simulation\\reinforcement_learning\\botzo_USD\\botzo_USD.usd",
+        usd_path="C:\\Users\\grego\\Desktop\\GRINGO\\botzo\\botzo\\CAD_files\\URDF\\BOTZO_URDF_description\\urdf\\BOTZO_URDF\\BOTZO_URDF.usd",
         activate_contact_sensors=True,
-        scale=(0.3, 0.3, 0.3),
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            retain_accelerations=False,
+            linear_damping=0.0,
+            angular_damping=0.0,
+            max_linear_velocity=1000.0,
+            max_angular_velocity=1000.0,
+            max_depenetration_velocity=1.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=False,
+            solver_position_iteration_count=12,
+            solver_velocity_iteration_count=4,
+        ),
     ),
     init_state=ArticulationCfg.InitialStateCfg(
+        pos=(0.0, 0.0, 0.5),
         joint_pos={
-            "BL_shoulder_joint": 0.0,
-            "BL_femur_joint": 0.0,
-            "BL_knee_joint": 0.0,
-
-            "BR_shoulder_joint": 0.0,
-            "BR_femur_joint": 0.0,
-            "Revolute_43": 0.0,
-
-            "FL_shoulder_joint": 0.0,
-            "FL_femur_joint": 0.0,
-            "FL_knee_joint": 0.0,
-
-            "FR_shoulder_joint": 0.0,
-            "FR_femur_joint": 0.0,
-            "FR_knee_joint": 0.0,
+            ".*HAA": 0.0,
+            ".*HFE": 0.8,
+            ".*KFE": 0.0,
+            ".*FOOT": 0.0,
         },
-        pos=(0.0, 0.0, 0.25),
-        # rotate 90 degrees around the x-axis
-        rot=(-0.7071, 0.0, 0.0, 0.7071),  # Quaternion for 90 degrees rotation around x-axis
-        # rotate upside down
-        #rot=(0.0, 1.0, 0.0, 0.0),
+        joint_vel={".*": 0.0},
     ),
+    soft_joint_pos_limit_factor=0.9,
     actuators={
-        "BL_shoulder_act": ImplicitActuatorCfg(joint_names_expr=["BL_shoulder_joint"],effort_limit_sim=effort_limit_sim, velocity_limit_sim=velocity_limit_sim, stiffness=stiffness, damping=damping),
-        "BL_femur_act": ImplicitActuatorCfg(joint_names_expr=["BL_femur_joint"], effort_limit_sim=effort_limit_sim, velocity_limit_sim=velocity_limit_sim, stiffness=stiffness, damping=damping),
-        "BL_knee_act": ImplicitActuatorCfg(joint_names_expr=["BL_knee_joint"], effort_limit_sim=effort_limit_sim, velocity_limit_sim=velocity_limit_sim, stiffness=stiffness, damping=damping),
-        "BR_shoulder_act": ImplicitActuatorCfg(joint_names_expr=["BR_shoulder_joint"], effort_limit_sim=effort_limit_sim, velocity_limit_sim=velocity_limit_sim, stiffness=stiffness, damping=damping),
-        "BR_femur_act": ImplicitActuatorCfg(joint_names_expr=["BR_femur_joint"], effort_limit_sim=effort_limit_sim, velocity_limit_sim=velocity_limit_sim, stiffness=stiffness, damping=damping),
-        "Revolute_43_act": ImplicitActuatorCfg(joint_names_expr=["Revolute_43"], effort_limit_sim=effort_limit_sim, velocity_limit_sim=velocity_limit_sim, stiffness=stiffness, damping=damping),
-        "FL_shoulder_act": ImplicitActuatorCfg(joint_names_expr=["FL_shoulder_joint"], effort_limit_sim=effort_limit_sim, velocity_limit_sim=velocity_limit_sim, stiffness=stiffness, damping=damping),
-        "FL_femur_act": ImplicitActuatorCfg(joint_names_expr=["FL_femur_joint"], effort_limit_sim=effort_limit_sim, velocity_limit_sim=velocity_limit_sim, stiffness=stiffness, damping=damping),
-        "FL_knee_act": ImplicitActuatorCfg(joint_names_expr=["FL_knee_joint"], effort_limit_sim=effort_limit_sim, velocity_limit_sim=velocity_limit_sim, stiffness=stiffness, damping=damping),
-        "FR_shoulder_act": ImplicitActuatorCfg(joint_names_expr=["FR_shoulder_joint"], effort_limit_sim=effort_limit_sim, velocity_limit_sim=velocity_limit_sim, stiffness=stiffness, damping=damping),
-        "FR_femur_act": ImplicitActuatorCfg(joint_names_expr=["FR_femur_joint"], effort_limit_sim=effort_limit_sim, velocity_limit_sim=velocity_limit_sim, stiffness=stiffness, damping=damping),
-        "FR_knee_act": ImplicitActuatorCfg(joint_names_expr=["FR_knee_joint"], effort_limit_sim=effort_limit_sim, velocity_limit_sim=velocity_limit_sim, stiffness=stiffness, damping=damping),
+        "legs": DCMotorCfg(
+            joint_names_expr=[".*HAA", ".*HFE", ".*KFE"],
+            effort_limit=60.0,
+            saturation_effort=80.0,
+            velocity_limit=10.0,
+            stiffness={".*": 10.0},
+            damping={".*": 0.2},
+            friction={".*": 0.05},
+        ),
     },
 )
